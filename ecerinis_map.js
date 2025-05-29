6120c37eed6f9b9b09dcfbdc8ed8a3e707efd2eb
Nitya Chagti <99576680+nchagti@users.noreply.github.com>
2025-05-29 16:13:43 -0400

    Delete ecerinis_map.js

--- ecerinis_map.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    console.log("DOM ready (shorthand)!");
-    // 1. Initialize the map and set its view
-    // L.map('map') creates a map instance linked to the div with id="map"
-   
-   
-    });
